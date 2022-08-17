42006ac6c9c402a2501dbe12e7ac632e0efeca68
dragopi <dragopi@gmail.com>
2022-08-17 23:46:55 +0200

    Add test for leader and strength calculation

--- src/TTACalc.test.tsx
+++ src/TTACalc.test.tsx
@@ -23,7 +23,7 @@ function GetEmptyScene() {
     return s;
 }
 
-function GetCardByCode(value: string, token: number = 0) {
+function GetCardByCode(value: string, token: number = 1) {
     let result: BoardCard = {
         code: value,
         workers: token
@@ -48,4 +48,55 @@ test('SceneCalculation InvalidScenario2', () => {
     s.Leader = GetCardByCode("LEA18");
     let result: TTASceneValues = TTASceneCalculation(s);
     expect(result.valid).toEqual(false);
+
 });
+
+test('SceneCalculation InvalidScenario3', () => {
+
+    let s: Scene = GetEmptyScene();
+
+    s.Age = 3;
+    s.Leader = GetCardByCode("LEA01");
+    let result: TTASceneValues = TTASceneCalculation(s);
+    expect(result.valid).toEqual(false);
+});
+
+test('SceneCalculation InvalidScenario4', () => {
+
+    let s: Scene = GetEmptyScene();
+    s.Age = 4;
+    s.Leader = GetCardByCode("LEA15");
+    let result: TTASceneValues = TTASceneCalculation(s);
+    expect(result.valid).toEqual(false);
+});
+
+/*
+test('SceneCalculation InvalidMilitaryArtillery', () => {
+
+    let s: Scene = GetEmptyScene();
+    s.Age = 1;
+    s.Artillery.push(GetCardByCode("MCA01"));
+    let result: TTASceneValues = TTASceneCalculation(s);
+    expect(result.valid).toEqual(false);
+});
+
+*/
+test('StrengthCalculation Scenario1', () => {
+
+    let s: Scene = GetEmptyScene();
+    s.Age = 2;
+    s.Leader = GetCardByCode("LEA15");
+    s.Infantry.push(GetCardByCode("MIN02",3));
+    s.Cavallery.push(GetCardByCode("MCA01",1));
+    s.Tactic = TTARepoCards.Instance.GetTactic("TAC02");
+    s.Urbans.push(GetCardByCode("UAR01", 1));
+    s.Special.push(GetCardByCode("SMI01"));
+    let result: TTASceneValues = TTASceneCalculation(s);
+    expect(result.valid).toEqual(true);
+    expect(result.strength.Value()).toEqual(16);
+
+    
+});
+
+
+
